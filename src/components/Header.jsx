541a812f8aa38d5c530a6c4ca91d91e809c6eb2d
Eun seong <8ohowb@naver.com>
2025-03-28 02:12:07 +0900

    헤더 영역 구조잡는중 에러 - 코드 누락해결

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,21 @@ import React from 'react'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header id='header'>
+      <div className='header_inner'>
+        <div className='header_logo'>
+          <a href='/'>portfolio</a>
+        </div>
+        <nav className='header_nav' aria-label='메인 메뉴'>
+          <ul>
+            <li><a href='#intro'>intro</a></li>
+            <li><a href='#skill'>skill</a></li>
+            <li><a href='#portfolio'>portfolio</a></li>
+            <li><a href='#contact'>contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 };
 
