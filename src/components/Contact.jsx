f5d9aaacf601ddf66efd5991a8f8b643992c153b
Eun seong <8ohowb@naver.com>
2025-04-24 00:42:14 +0900

    footer에 사이트에 대한 간단설명작성 후 contact 임시 구조기반

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,8 +3,28 @@ import React, { forwardRef }from 'react'
 const Contact = forwardRef((props, ref) => {
   return (
     <section className='contact_section' ref={ref} id='contact'>
-      <h2>저의 연락처입니다</h2>
-      <div>Contact</div>
+      <h2 className='contact_title'>감사합니다</h2>
+      <p className='contact_subtitle'>
+        추가적으로 궁금한 점이 있다면 <br />
+        여기로 연락주세요
+      </p>
+
+      <div className='contact_box'>
+        <div className='contact_item'>
+          <span className='label'>연락처</span>
+          <span className='value'>010-2793-4856</span>
+        </div>
+        <div className='contact_item'>
+          <span className='label'>이메일</span>
+          <span className='value'>8ohowb@gmail.com</span>
+        </div>
+        <div className='contact_item'>
+          <span className='label'>깃허브</span>
+          <span className='value'><a href='https://github.com/SilverStarGo' target='_blank' rel='noopener noreferrer'>@SilverStarGo</a></span>
+          {/* target='_blank' : 링크가 새창에서 열리지만 보안에 취약 */}
+          {/* rel='noopener noreferrer' : 새 탭이 원본 창의 객체 속성에 액세스하는 것을 방지 + 리퍼러 정보가 새 탭으로 전송되지 않도록 보장 */}
+        </div>
+      </div>
     </section>
 
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <h4>이 사이트는 리액트를 이용하여 제작된 비상업적 포트폴리오입니다.</h4>
   );
 };
 
