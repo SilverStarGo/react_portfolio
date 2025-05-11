a7d975d5931c41fb8ecbc50e3b73407283e479fb
Eun seong <8ohowb@naver.com>
2025-05-11 15:50:09 +0900

    project 데이터 정리 후 modal 기능, Portfolio 부분 수정 완료

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,36 +1,5 @@
 import React, { useState , forwardRef} from 'react';
-import css from '../assets/img/css.png';
-
-const skillIconMap = {
-  skill1: css,
-  skill2: css,
-  skill3: css,
-  skill4: css,
-};
-
-const projects = [
-  {
-    id: 1,
-    title: '프로젝트명1',
-    description: '간단설명1',
-    skills:['skill1', 'skill2', 'skill3', 'skill4'],
-    bgColor: '#111',
-  },
-  {
-    id: 2,
-    title: '프로젝트명2',
-    description: '간단설명2',
-    skills:['skill1', 'skill2', 'skill3', 'skill4'],
-    bgColor: '#1e1e2f',
-  },
-  {
-    id: 3,
-    title: '프로젝트명3',
-    description: '간단설명3',
-    skills:['skill1', 'skill2', 'skill3', 'skill4'],
-    bgColor: '#111',
-  },
-];
+import { projectData } from '../data/projectdata.js';
 
 const Portfolio = forwardRef((props, ref) => {
   const [modalProject, setModalProject] = useState(null);
@@ -41,20 +10,21 @@ const Portfolio = forwardRef((props, ref) => {
       <p className='portfolio_section_subtitle'>카드를 클릭하면 상세 정보를 볼 수 있어요.</p>
 
       <div className='portfolio_grid'>
-        {projects.map((project) => (
+        {projectData.map((project) => (
           <div
             key={project.id}
             className='project_card'
             style={{ backgroundColor: project.bgColor }}
             onClick={() => setModalProject(project)}
           >
+            <img src={project.thumbnail} alt={project.title} className='projectThumb' />
             <h3>{project.title}</h3>
-            <p>{project.description}</p>
+            <p>{project.shortDesc}</p>
             <div className='skill_icons'>
               {project.skills.map((skill, index) => (
                 <img key={index}
-                src={skillIconMap[skill]}
-                alt={skill}
+                src={skill}
+                alt={`skill-${index}`}
                 className="skill_icon" />
               ))}
             </div>
@@ -68,12 +38,22 @@ const Portfolio = forwardRef((props, ref) => {
           <div className='modal_content' onClick={(e) => e.stopPropagation()}>
             <h3>{modalProject.tilte}</h3>
             <p>{modalProject.description}</p>
+            <div className='modalMeta'>
+              <p><strong>참여인원:</strong> {modalProject.members}</p>
+              <p><strong>기간:</strong> {modalProject.period}</p>
+              <p><strong>관련 링크:</strong> <a href={modalProject.link} target="_blank" rel="noopener noreferrer">바로가기</a></p>
+            </div>
+            <ul className="modalDetails">
+              {modalProject.details.map((detail, index) => (
+                <li key={index}>{detail}</li>
+              ))}
+            </ul>
             <p><strong>사용 기술</strong> 
             <div className='modal_skill_icons'>
               {modalProject.skills.map((skill, index) => (
               <img key={index}
-              src={skillIconMap[skill]}
-              alt={skill}
+              src={skill}
+              alt={`skill-${index}`}
               className='modal_skill_icon'
               />
             ))}
---
+++ src/data/projectdata.js
@@ -0,0 +1,95 @@
+import android from "../assets/img/android.png";
+import bootstrap from "../assets/img/bootstrap.png";
+import css from "../assets/img/css.png";
+import eclipse from "../assets/img/eclipse.png";
+import figma from "../assets/img/figma.png";
+import firebase from "../assets/img/firebase.png";
+import git from "../assets/img/git.png";
+import github from "../assets/img/github.png";
+import html from "../assets/img/html.png";
+import intellij from "../assets/img/intellij.png";
+import java from "../assets/img/java.png";
+import jpa from "../assets/img/jpa.png";
+import jquery from "../assets/img/jquery.png";
+import js from "../assets/img/js.png";
+import kotlin from "../assets/img/kotlin.png";
+import mybatis from "../assets/img/mybatis.png";
+import mysql from "../assets/img/mysql.png";
+import netlify from "../assets/img/netlify.png";
+import nodejs from "../assets/img/nodejs.png";
+import oracle from "../assets/img/oracle.png";
+import photoshop from "../assets/img/photoshop.png";
+import react from "../assets/img/react.png";
+import sass from "../assets/img/sass.png";
+import spring from "../assets/img/spring.png";
+import tomcat from "../assets/img/tomcat.png";
+import vsc from "../assets/img/vsc.png";
+import vuejs from "../assets/img/vuejs.png";
+
+export const projectData = [
+{
+    id: 1,
+    title: "tomcat 기반 웹사이트 개발 및 유지보수",
+    thumbnail: html,        // 카드 썸네일
+    skills: [html, js, react, css], // 기술 스킬
+    shortDesc: "머시기저시기 가구 프로젝트", // 카드용 요약 설명
+    period: "2022.~",
+    members: "5명",
+    link: "http",
+    description: "머시기 저시기에서 한 가구쇼핑몰 프로젝트",
+    details: [
+      "머시기의 일정",
+      "데이터연동",
+      "회원정보 보안 ",
+      "Github 기반 협업 환경 운영 및 코드 리뷰",
+    ]    
+},
+{
+    id: 2,
+    src: react,
+    title: "프로젝트2",
+    content: "캠핑 앱",
+    skills: [react, nodejs, firebase],
+  },
+  {
+    id: 3,
+    src: vuejs,
+    title: "프로젝트3",
+    content: "블로그 웹사이트입니다.",
+    skills: [vuejs, sass, git],
+  },
+  {
+    id: 4,
+    title: "tomcat 기반 웹사이트 개발 및 유지보수",
+    thumbnail: html,        // 카드 썸네일
+    skills: [html, js, react, css], // 기술 스킬
+    shortDesc: "머시기저시기 가구 프로젝트트", // 카드용 요약 설명
+    period: "2022.~",
+    members: "5명",
+    link: "http",
+    description: "머시기 저시기에서 한 가구쇼핑몰 프로젝트트",
+    details: [
+      "머시기의 일정정",
+      "데이터연동동",
+      "회원정보 보안 ",
+      "Github 기반 협업 환경 운영 및 코드 리뷰",
+    ]
+  },
+  {
+    id: 5,
+    title: "tomcat 기반 웹사이트 개발 및 유지보수",
+    thumbnail: html,        // 카드 썸네일
+    skills: [html, js, react, css], // 기술 스킬
+    shortDesc: "머시기저시기 가구 프로젝트트", // 카드용 요약 설명
+    period: "2022.~",
+    members: "5명",
+    link: "http",
+    description: "머시기 저시기에서 한 가구쇼핑몰 프로젝트트",
+    details: [
+      "머시기의 일정정",
+      "데이터연동동",
+      "회원정보 보안 ",
+      "Github 기반 협업 환경 운영 및 코드 리뷰",
+    ]
+  },
+]
