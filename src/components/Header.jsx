bae601ddf0a8a68747aff912f3ce8a810f448436
Eun seong <8ohowb@naver.com>
2025-05-12 16:09:33 +0900

    헤더 영역 navbar 수정작업 진행

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 
 const Header = ({onIntroClick, onSkillClick, 
   onPortfolioClick, onSelfstudyClick, onContactClick}) => {
+
+  const [activeTab, setActiveTab] = React.useState('intro');
+
+  const handleClick = (section, callback) => {
+    setActiveTab(section);
+    callback();
+  }
+
   return (
     <header id='header'>
       <div className='header_inner'>
@@ -10,11 +18,11 @@ const Header = ({onIntroClick, onSkillClick,
         </div>
         <nav className='header_nav' aria-label='메인 메뉴'>
           <ul>
-            <li><button onClick={onIntroClick}>intro</button></li>
-            <li><button onClick={onSkillClick}>skill</button></li>
-            <li><button onClick={onPortfolioClick}>portfolio</button></li>
-            <li><button onClick={onSelfstudyClick}>selfstudy</button></li>
-            <li><button onClick={onContactClick}>contact</button></li>
+            <li><button className={activeTab === 'intro' ? 'active' : ''}onClick={onIntroClick}>intro</button></li>
+            <li><button className={activeTab === 'skill' ? 'active' : ''}onClick={onSkillClick}>skill</button></li>
+            <li><button className={activeTab === 'portfolio' ? 'active' : ''}onClick={onPortfolioClick}>portfolio</button></li>
+            <li><button className={activeTab === 'selfstudy' ? 'active' : ''}onClick={onSelfstudyClick}>selfstudy</button></li>
+            <li><button className={activeTab === 'contact' ? 'active' : ''}onClick={onContactClick}>contact</button></li>
           </ul>
         </nav>
       </div>
