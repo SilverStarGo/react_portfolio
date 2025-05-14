6644c9b8961a78dee27a9b4d0a9ad54834ee567e
Eun seong <8ohowb@naver.com>
2025-05-15 00:49:03 +0900

    인트로 영역 수정 및 불필요한 부분 삭제, 각각 섹션 배경컬러 조정

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,19 +1,16 @@
 import React, { forwardRef } from 'react';
-import about from "../assets/img/computer.jpg";
 
 
 
 const Intro = forwardRef((props, ref) => {
   return (
       <section className='intro_content' ref={ref} id='intro'>
-        <div className='intro_img'>
-          <img src={about} alt='사진' />
-        </div>
-        <h1>Developer</h1>
+        <h1>안녕하세요.</h1>
+        <h1>모르는 것을 두려워하지 않고, <br/> 직접 만들며 배우는 프론트엔드 개발자 정은성입니다.</h1>
         <div className='intro_text'>
-          <p>저는 정은성입니다.</p>
-          <p>자기소개하기</p>
-          <p>마지막 각오까지</p>
+          <p>작은 도전이 모여</p>
+          <p>저를 성장시킨다는 믿음으로</p>
+          <p>매일 한 걸음씩 나아가고 있습니다.</p>
         </div>
       </section>
   );
